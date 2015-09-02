c1e8ae97f055bd7bc1b6d6b4da6991d0df399ffc
stirlingw <Lyman123>
2015-09-02 16:54:10 -0600

    adding to server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-app.use(express.static('dist'));
+//app.use(express.static('dist'));
+app.use(express.static(__dirname + '/build'));
+
 app.get('/', function (req, res) {
     res.sendFile('index.html');
 });
--- webpack.devServer.js
+++ webpack.devServer.js
@@ -12,16 +12,3 @@ webserver = new server( compiler, { } );
 webserver.listen(9000, '0.0.0.0', function () { });
 
 console.log('Running on port 9000');
-
-
-var express = require('express');
-var app = express();
-app.use(express.static('dist'));
-app.get('/', function (req, res) {
-    res.sendFile('index.html');
-});
-var port = process.env.PORT || 3000;
-var server = app.listen(port, function () {
-    var host = server.address().address;
-    console.log('Fit.io app listening at: ', port);
-});
