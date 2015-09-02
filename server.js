16c50d8908b41f13090177a40362b2736af7d174
stirlingw <Lyman123>
2015-09-02 16:37:45 -0600

    fixing stuff for server

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+app.use(express.static('dist'));
+app.get('/', function (req, res) {
+    res.sendFile('index.html');
+});
+var port = process.env.PORT || 9000;
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    console.log('StirlingWaiteCom app listening at: ', port);
+});
